88b940d93b22d6ff0705d88867246b89f6d9c313
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-16 04:31:24 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 //exec('ffmpeg -i "https://poty-dl.herokuapp.com/409/k.mp4" -ss 00:00:10 -vframes 1 -f image2 "image.jpg"',$output);
 exec('ffmpeg -i "https://poty-dl.herokuapp.com/409/k.mp4" -map 0:v -ss 00:00:05 -frames:v 1 frame_1.png -map 0:v -ss 00:00:10 -frames:v 1 frame_2.png',$output);
+
+echo '<img src="data:image/png;base64, ';
 echo base64_encode(file_get_contents('frame_1.png'));
-
-echo '<br><br>';
-
-echo base64_encode(file_get_contents('frame_2.png'));
+echo '"/>';
 ?>
