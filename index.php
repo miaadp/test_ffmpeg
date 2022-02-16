98bafdffa76a28c9a32f554ad868b7ebf2019cd3
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-16 04:21:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 exec('ffmpeg -i "https://poty-dl.herokuapp.com/408/k.mp4" -ss 00:00:10 -vframes 1 -f image2 "image.jpg"',$output);
+header("Content-type: image/jpeg");
 echo file_get_contents('image.jpg');
 ?>
