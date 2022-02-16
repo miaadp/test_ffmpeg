d85e69999ddf8c5ec9f48b5d394f34341859c82b
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-17 02:24:10 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
-exec('ffmpeg -ss 00:10:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-1.jpg"',$output);
-exec('ffmpeg -ss 00:20:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-2.jpg"',$output);
-exec('ffmpeg -ss 00:30:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-3.jpg"',$output);
-exec('ffmpeg -ss 00:40:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-4.jpg"',$output);
-exec('ffmpeg -ss 00:50:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-5.jpg"',$output);
-
-$res = glob("img-*.jpg");
-$datas = [];
-foreach ($res as $file){
-    $datas[] = base64_encode(file_get_contents($file));
-    unlink($file);
-}
-echo json_encode($datas);
+use thiagoalessio\TesseractOCR\TesseractOCR;
+echo (new TesseractOCR('http://ez-dl.ir/p.jpg'))->run();
