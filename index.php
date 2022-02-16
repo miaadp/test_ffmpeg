e4b27aa4608280a26ee43518c450fbc88524497a
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-16 04:06:10 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-
+if(!file_exists("k.mp4")){
+  file_put_contents("https://poty-dl.herokuapp.com/408/k.%40PotyServices.mp4","k.mp4");
+}
+$movie = new ffmpeg_movie("k.mp4");
+var_dump($movie);
 ?>
