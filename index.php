25b59daf011c62adebbc930b8af35066b1f478e6
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-17 01:05:56 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
+exec('ffmpeg -ss 00:5:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-1.jpg"',$output);
+exec('ffmpeg -ss 00:10:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-2.jpg"',$output);
+exec('ffmpeg -ss 00:15:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-3.jpg"',$output);
+exec('ffmpeg -ss 00:20:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-4.jpg"',$output);
 exec('ffmpeg -ss 00:45:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-5.jpg"',$output);
-header("Content-type: application/zip");
+
 $res = glob("img-*.jpg");
-$zip = new ZipArchive();
-$zip->open('images.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+$datas = [];
 foreach ($res as $file){
-    $zip->addFile($file);
-    unlink($file);
+    $datas[] = base64_encode(file_get_contents($file));
 }
-$zip->close();
-echo readfile('images.zip');
+echo json_encode($datas);
