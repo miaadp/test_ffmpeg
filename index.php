1a5ce9f83194cf5f0d81025a0818a1c5b53c8427
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-17 02:42:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 use thiagoalessio\TesseractOCR\TesseractOCR;
 require 'vendor/autoload.php';
-echo (new TesseractOCR('http://ez-dl.ir/p.jpg'))->run();
+echo (new TesseractOCR('photo_2022-02-15_19-47-26.jpg'))->run();
