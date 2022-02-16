76cfea10aac19da988103b9a966f728511adb457
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-17 01:25:57 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-ini_set('max_execution_time',120);
-exec('ffmpeg -ss 00:7:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-1.jpg"',$output);
-exec('ffmpeg -ss 00:14:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-2.jpg"',$output);
-exec('ffmpeg -ss 00:21:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-3.jpg"',$output);
-exec('ffmpeg -ss 00:28:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-4.jpg"',$output);
-exec('ffmpeg -ss 00:35:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-5.jpg"',$output);
-exec('ffmpeg -ss 00:42:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-6.jpg"',$output);
-exec('ffmpeg -ss 00:49:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-7.jpg"',$output);
+exec('ffmpeg -ss 00:10:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-1.jpg"',$output);
+exec('ffmpeg -ss 00:20:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-2.jpg"',$output);
+exec('ffmpeg -ss 00:30:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-3.jpg"',$output);
+exec('ffmpeg -ss 00:40:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-4.jpg"',$output);
+exec('ffmpeg -ss 00:50:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-5.jpg"',$output);
 
 $res = glob("img-*.jpg");
 $datas = [];
