f85028c0a7d867a9eed83d824170d7bca5ffe640
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-02-17 00:54:06 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-//exec('ffmpeg -ss 00:05:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "image.jpg"');
-//exec('ffmpeg -ss 00:15:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "image2.jpg"');
-//exec('ffmpeg -ss 00:25:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "image3.jpg"');
-//exec('ffmpeg -i "https://poty-dl.herokuapp.com/409/k.mp4" -map 0:v -ss 00:00:10 -frames:v 1 frame_1.png -map 0:v -ss 00:05:10 -frames:v 1 frame_2.png',$output);
+exec('ffmpeg -ss 00:25:10 -i "https://poty-dl.herokuapp.com/409/k.mp4" -vframes 1 -f image2 "img-3.jpg"',$output);
+
 header("Content-type: application/zip");
-exec('ffmpeg -i "https://poty-dl.herokuapp.com/409/k.mp4" -vf fps=1/300 img-%%02d.jpg');
 $res = glob("img-*.jpg");
 $zip = new ZipArchive();
 $zip->open('images.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
@@ -13,4 +10,3 @@ foreach ($res as $file){
 }
 $zip->close();
 echo readfile('images.zip');
-?>
