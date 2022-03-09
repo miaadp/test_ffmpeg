e8293db04a293a241e402755820eef6755348337
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-03-10 00:40:54 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-exec("/usr/local/bin/ffmpeg -i https://ez-dl.herokuapp.com/5d9 -b:v 700k -bufsize 700k output.mp4";);
+exec("/usr/local/bin/ffmpeg -i https://ez-dl.herokuapp.com/5d9 -b:v 700k -bufsize 700k output.mp4");
