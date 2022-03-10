16c00358e7b1a89d0146d7f32e355765174946da
Miaad Sadeghloo <73884897+miaadp@users.noreply.github.com>
2022-03-11 02:36:27 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-exec("ffmpeg -i https://ez-dl.herokuapp.com/5d9 -preset ultrafast -threads 2  -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? output.mp4 -y");
+exec("ffmpeg -i https://ez-dl.herokuapp.com/5d9 -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? output.mp4 -y");
